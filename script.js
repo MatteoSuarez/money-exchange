685fdf783be9633f6dc10f22221e2860edb130c7
Matteo Suarez <matteosuarezc@gmail.com>
2023-03-13 16:07:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let entryDevise = document.getElementById("select_1");
+let exitDevise = document.getElementById("select_2");
+
+let dollarMontly = document.getElementById("dollar_montly")
+let euroMontly = document.getElementById("euro_montly")
